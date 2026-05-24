f2f446aa94a78d2acc0369045cbbea649b7e5f8d
Sapphire <maynardjonathan4@gmail.com>
2026-05-24 20:43:23 +0700

    revisi terbaru lagi

--- app/src/main/java/com/example/mondu/KelolaUserFragment.kt
+++ app/src/main/java/com/example/mondu/KelolaUserFragment.kt
@@ -44,10 +44,8 @@ class KelolaUserFragment : Fragment() {
 
         rvUsers.layoutManager = LinearLayoutManager(context)
 
-        // Setup Filter Popup
         btnFilterUsers.setOnClickListener { showFilterMenu(it) }
 
-        // Setup Search Logic
         etSearchUsers.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
